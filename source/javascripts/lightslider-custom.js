463c3229e8ff3e83e4fc470b6909e201629b4d6a
Tyler Pottle <tyler.pottle@gmail.com>
2015-07-07 14:06:34 -0700

    custom slider fuction for slideshow

---
+++ source/javascripts/lightslider-custom.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $("#lightSlider").lightSlider({
+    gallery: true,
+    item: 1,
+    thumbitem: 3,
+    slideMargin: 0,
+    speed:500,
+    auto:true,
+    loop:true,
+    autoWidth: false
+  });
+});
